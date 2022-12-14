48853117339f65c80a106d4bff951174a6576676
KreoX <soldimksol@gmail.com>
2022-12-14 18:29:48 +0300

    Commit

---
+++ Laptop.java
@@ -0,0 +1,143 @@
+public class Laptop {
+    private String brand;
+    private String model;
+    private int resolution;
+    private int frequency;
+    private int coresNum;
+    private String driveType;
+    private int driveSize;
+    private int RAM;
+    private int videoMem;
+    private int price;
+
+    public Laptop(String brand, String model, int resolution, int frequency, int coresNum, String driveType,
+            int driveSize, int rAM, int videoMem, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.resolution = resolution;
+        this.frequency = frequency;
+        this.coresNum = coresNum;
+        this.driveType = driveType;
+        this.driveSize = driveSize;
+        RAM = rAM;
+        this.videoMem = videoMem;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public int getCoresNum() {
+        return coresNum;
+    }
+
+    public String getDriveType() {
+        return driveType;
+    }
+
+    public int getDriveSize() {
+        return driveSize;
+    }
+
+    public void setDriveSize(int driveSize) {
+        this.driveSize = driveSize;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(int rAM) {
+        RAM = rAM;
+    }
+
+    public int getVideoMem() {
+        return videoMem;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop [brand=" + brand + ", model=" + model + ", resolution=" + resolution + ", frequency=" + frequency
+                + ", coresNum=" + coresNum + ", driveType=" + driveType + ", driveSize=" + driveSize + ", RAM=" + RAM
+                + ", videoMem=" + videoMem + ", price=" + price + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + resolution;
+        result = prime * result + frequency;
+        result = prime * result + coresNum;
+        result = prime * result + ((driveType == null) ? 0 : driveType.hashCode());
+        result = prime * result + driveSize;
+        result = prime * result + RAM;
+        result = prime * result + videoMem;
+        result = prime * result + price;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (brand == null) {
+            if (other.brand != null)
+                return false;
+        } else if (!brand.equals(other.brand))
+            return false;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (resolution != other.resolution)
+            return false;
+        if (frequency != other.frequency)
+            return false;
+        if (coresNum != other.coresNum)
+            return false;
+        if (driveType == null) {
+            if (other.driveType != null)
+                return false;
+        } else if (!driveType.equals(other.driveType))
+            return false;
+        if (driveSize != other.driveSize)
+            return false;
+        if (RAM != other.RAM)
+            return false;
+        if (videoMem != other.videoMem)
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    }
+}
---
+++ master.java
@@ -0,0 +1,259 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class master {
+    public static void main(String[] args) {
+        ArrayList<Laptop> laptops = new ArrayList<Laptop>();
+        Laptop laptop = new Laptop("ASUS", "ROG Strix G15", 1080, 144, 8,
+                "SSD", 512, 16, 4, 1100);
+        laptops.add(laptop);
+        laptop = new Laptop("Lenovo", "IdeaPad Gaming 3", 1080, 60, 6,
+                "SSD", 512, 8, 4, 850);
+        laptops.add(laptop);
+        laptop = new Laptop("Lenovo", "Legion 5", 1080, 144, 8,
+                "SSD", 512, 16, 6, 2100);
+        laptops.add(laptop);
+        laptop = new Laptop("Apple", "Macbook Air 13", 1600, 60, 8,
+                "SSD", 256, 8, 0, 1000);
+        laptops.add(laptop);
+        laptop = new Laptop("HP", "250 G8", 768, 60, 2,
+                "HDD", 500, 4, 0, 500);
+        laptops.add(laptop);
+        laptop = new Laptop("Acer", "Nitro 5", 1080, 144, 6,
+                "HDD+SSD", 1256, 16, 4, 1200);
+        laptops.add(laptop);
+        laptop = new Laptop("Apple", "Macbook Pro 14", 1964, 120, 10,
+                "SSD", 1024, 32, 0, 5000);
+        laptops.add(laptop);
+        laptop = new Laptop("Digma", "Eve 15", 1080, 60, 4,
+                "SSD", 128, 4, 0, 300);
+        laptops.add(laptop);
+
+        while (true) {
+            System.out.println("Для вывода всех доступных ноутбуков введите 1:");
+            System.out.println("Для перехода к фильтрации введите 2:");
+            System.out.println("Для выхода из программы введите 3:");
+            Scanner scanner = new Scanner(System.in);
+            String mode = scanner.nextLine();
+            if (mode.equals("1")) {
+
+                for (Laptop lap : laptops) {
+                    System.out.println(lap);
+
+                }
+            }
+            if (mode.equals("2")) {
+                filtring(laptops);
+            }
+            if (mode.equals("3")) {
+                break;
+            } else {
+                System.out.println("Проверьте правильность ввода");
+            }
+
+        }
+
+    }
+
+    public static void filtring(ArrayList<Laptop> laptops) {
+        Scanner scanner = new Scanner(System.in);
+
+        String brandFilter = "vawkfwef234";
+        while (brandFilter == "vawkfwef234") {
+            System.out.println("Для выбора марки введите следующее: ASUS-1, Lenovo-2, Apple-3, HP-4, Acer-5, Digma-6");
+            System.out.println("Если хотите пропустить этот вопрос, введите 0:");
+            String temp = scanner.nextLine();
+            switch (temp) {
+                case "0":
+                    brandFilter = null;
+                    break;
+                case "1":
+                    brandFilter = "ASUS";
+                    break;
+                case "2":
+                    brandFilter = "Lenovo";
+                    break;
+                case "3":
+                    brandFilter = "Apple";
+                    break;
+                case "4":
+                    brandFilter = "HP";
+                    break;
+                case "5":
+                    brandFilter = "Acer";
+                    break;
+                case "6":
+                    brandFilter = "Digma";
+                    break;
+
+            }
+
+        }
+        int resolutionFilter = 12313131;
+        while (resolutionFilter == 12313131) {
+            System.out.println("Введите минимальное разрешение экрана (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                resolutionFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (resolutionFilter < 0) {
+                resolutionFilter = 12313131;
+                System.out.println("Разрешение не может быть отрицательным!");
+            }
+        }
+        int frequencyFilter = 12313131;
+        while (frequencyFilter == 12313131) {
+            System.out.println(
+                    "Введите минимальную частоту обновления экрана, Гц (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                frequencyFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (frequencyFilter < 0) {
+                frequencyFilter = 12313131;
+                System.out.println("Частота обновления не может быть отрицательной!");
+            }
+        }
+        int coresNumFilter = 12313131;
+        while (coresNumFilter == 12313131) {
+            System.out
+                    .println("Введите минимальное количество ядер процессора (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                coresNumFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (coresNumFilter < 0) {
+                coresNumFilter = 12313131;
+                System.out.println("количество ядер не может быть отрицательным!");
+            }
+        }
+        String driveTypeFilter = "vawkfwef234";
+        while (driveTypeFilter == "vawkfwef234") {
+            System.out.println("Для выбора типа накопителя введите следующее: SSD -1, HDD -2, SSD+HDD -3");
+            System.out.println("Если хотите пропустить этот вопрос, введите 0:");
+            String temp = scanner.nextLine();
+            switch (temp) {
+                case "0":
+                    driveTypeFilter = null;
+                    break;
+                case "1":
+                    driveTypeFilter = "SSD";
+                    break;
+                case "2":
+                    driveTypeFilter = "HDD";
+                    break;
+                case "3":
+                    driveTypeFilter = "SSD+HDD";
+                    break;
+
+            }
+
+        }
+        int driveSizeFilter = 12313131;
+        while (driveSizeFilter == 12313131) {
+            System.out.println("Введите минимальный объем накопителя, ГБ (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                driveSizeFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (driveSizeFilter < 0) {
+                driveSizeFilter = 12313131;
+                System.out.println("Объем накопителя не может быть отрицательным!");
+            }
+        }
+
+        int RamFilter = 12313131;
+        while (RamFilter == 12313131) {
+            System.out.println(
+                    "Введите минимальный объем оперативной памяти, ГБ (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                RamFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (RamFilter < 0) {
+                RamFilter = 12313131;
+                System.out.println("Объем оперативной памяти не может быть отрицательным!");
+            }
+        }
+
+        int videoMemFilter = 12313131;
+        while (videoMemFilter == 12313131) {
+            System.out.println("Введите минимальный объем видеопамяти, ГБ (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                videoMemFilter = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (videoMemFilter < 0) {
+                videoMemFilter = 12313131;
+                System.out.println("Объем видеопамяти не может быть отрицательным!");
+            }
+        }
+        int maxPrice = 12313131;
+        while (maxPrice == 12313131) {
+            System.out.println("Введите максимальную стоимость ноутбука, $ (0-если желаете пропустить этот вопрос):");
+            String temp = scanner.nextLine();
+            try {
+                maxPrice = Integer.parseInt(temp);
+
+            } catch (Exception e) {
+                System.out.println("Проверьте правильность ввода!");
+            }
+            if (maxPrice < 0) {
+                maxPrice = 12313131;
+                System.out.println("Цена не может быть отрицательной (к сожалению)!");
+            }
+        }
+        System.out.println("По заданным критериям отобраны следующие модели:");
+        for (Laptop lap : laptops) {
+            if (brandFilter != null && !brandFilter.equals(lap.getBrand())) {
+                continue;
+            }
+            if (resolutionFilter > lap.getResolution()) {
+                continue;
+            }
+            if (frequencyFilter > lap.getFrequency()) {
+                continue;
+            }
+            if (coresNumFilter > lap.getCoresNum()) {
+                continue;
+            }
+            if (driveTypeFilter != null && !driveTypeFilter.equals(lap.getDriveType())) {
+                continue;
+            }
+            if (driveSizeFilter > lap.getDriveSize()) {
+                continue;
+            }
+            if (RamFilter > lap.getRAM()) {
+                continue;
+            }
+            if (videoMemFilter > lap.getVideoMem()) {
+                continue;
+            }
+            if (maxPrice != 0 && maxPrice < lap.getPrice()) {
+                continue;
+            }
+            System.out.println(lap);
+
+        }
+
+    }
+
+}
